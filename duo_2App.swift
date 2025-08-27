a992a6579fcbcf62051fb6e86623d171f0568682
dev <47551725+devadhathan@users.noreply.github.com>
2025-08-27 19:57:37 +0100

    Add files via upload

---
+++ duo_2App.swift
@@ -0,0 +1,21 @@
+//
+//  duo_2App.swift
+//  duo 2
+//
+//  Created by Devdhathan M D on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct duo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+#Preview{
+    ContentView()
+
+}
